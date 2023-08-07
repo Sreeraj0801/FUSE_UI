c9b42a8321fa276b8a3e14528b8491c3ab87d6e2
Sreeraj R <sreeraj.r.0801@gmail.com>
2023-08-07 17:45:00 +0530

    Landing page UI

---
+++ src/app/shared/nav-button/nav-button.component.ts
@@ -0,0 +1,12 @@
+import { Component ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-button',
+  templateUrl: './nav-button.component.html',
+  styleUrls: ['./nav-button.component.css']
+})
+export class NavButtonComponent {
+@Input() text ?: string;
+@Input() design ?:string;
+@Input() path ?:string;
+}
