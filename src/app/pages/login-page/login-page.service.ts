8e87960afa157a662cf00720a9e7b369d756c93f
Sreeraj R <sreeraj.r.0801@gmail.com>
2023-08-09 18:37:09 +0530

    Removed any type

--- src/app/pages/login-page/login-page.service.ts
+++ src/app/pages/login-page/login-page.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environmet } from 'src/environment/environment';
+import { environment } from 'src/environment/environment';
 import { LoginDetailsInterface, LoginResInterface } from 'src/app/config/config.types';
 
 @Injectable({
@@ -11,11 +11,11 @@ export class LoginPageService {
   constructor(private http:HttpClient) { }
 
   login(details:LoginDetailsInterface){
-  return this.http.post<LoginResInterface>(environmet.baseUrl+'login',details);
+  return this.http.post<LoginResInterface>(environment.baseUrl+'login',details);
 }
 
 googleLogin(email:string){
-  return this.http.post<LoginResInterface>(environmet.baseUrl + 'googleSignIn',{email});
+  return this.http.post<LoginResInterface>(environment.baseUrl + 'googleSignIn',{email});
 }
 
 
